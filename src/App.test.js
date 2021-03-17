c45c5a11280dcd402f0533a4eb690abe6d4ad9b1
nuttylove <nutty.pansila@gmail.com>
2021-03-18 06:37:27 +0700

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/My Shop/i);
   expect(linkElement).toBeInTheDocument();
 });
