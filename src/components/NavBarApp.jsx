88a7b804913ca63c5f3430f476f1d25bce1ee96f
chotika <nutty.pansila@gmail.com>
2021-03-19 20:48:02 +0700

    header

--- src/components/NavBarApp.jsx
+++ src/components/NavBarApp.jsx
@@ -6,17 +6,20 @@ import { withRouter } from 'react-router-dom';
 const NavBarApp = ({ history }) => {
 	const { cart } = useSelector(state => state.app);
 	return (
+		<div className='navbar-header'>
 		<Navbar appearance='subtle'>
-			<Navbar.Header className='flex items-center justify-center'>
+			{/* <Navbar.Header className='flex items-center justify-center'>
       	<img src={logo} width='50' height='50' alt='logo' />
-			</Navbar.Header>
+			</Navbar.Header> */}
         <Navbar.Body>
+				<Nav><img src={logo} width='50' height='50' alt='logo' /></<Nav>
 				<Nav pullRight>
 					<Nav.Item icon={<Icon icon='shopping-cart' size="2x" />} onClick={() => history.push('/cart')}>Cart ( {cart} )</Nav.Item>
 					<Nav.Item icon={<Icon icon='bars' size="2x" />} />
 				</Nav>
         </Navbar.Body>
 		</Navbar>
+		</div>
 	);
 };
 
