bff21049b94992c1dae6607952775caa96f36050
chotika <nutty.pansila@gmail.com>
2021-03-19 11:52:18 +0700

    Size

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -7,10 +7,15 @@ const ProductDetail = () => {
   const { name } = useParams();
   const [img, setImg] = useState(null);
   const [quantity, setQuantity] = useState(1);
+  const [size, setSize] = useState('S');
+  const [color, setColor] = useState('Red');
 
   useEffect(() => {
     setImg({ ...ImageList.find(f => f.props.alt === name).props });
-    return () => setImg(null);
+    return () => {
+      setImg(null);
+      setColor('red');
+    };
   }, [name]);
 
   return (
@@ -19,14 +24,14 @@ const ProductDetail = () => {
       <img {...img} className="detail-img" alt='detail' />
       <span>Name <span>{name}</span></span>
       <span>Color 
-        <Button color="red" appearance="ghost">Red</Button>
-        <Button color="blue" appearance="ghost">Blue</Button>
-        <Button color="green" appearance="ghost">Green</Button>
+        <Button onClick={() => setColor('Red')} className='bg-back' color="red" appearance="ghost">Red</Button>
+        <Button onClick={() => setColor('Blue')} className='bg-back' color="blue" appearance="ghost">Blue</Button>
+        <Button onClick={() => setColor('Green')} className='bg-back' color="green" appearance="ghost">Green</Button>
       </span>
       <span>Size 
-        <Button appearance="ghost">S</Button>
-        <Button appearance="ghost">M</Button>
-        <Button appearance="ghost">L</Button>
+        <Button onClick={() => setSize('S')}>S</Button>
+        <Button onClick={() => setSize('M')}>M</Button>
+        <Button onClick={() => setSize('L')}>L</Button>
       </span>
       <span>Quantity 
         <Button appearance="ghost" onClick={() => setQuantity(n => n + 1)}>+</Button>
