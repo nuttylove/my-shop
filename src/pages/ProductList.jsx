ef0d11dcbafce10be024382e49b22012f24f096e
chotika <nutty.pansila@gmail.com>
2021-03-19 11:21:09 +0700

    link

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import Slide from '../components/Slide';
 import ImageList from '../components/ImageList';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
-const ProductList = ({ history }) => {
+const ProductList = () => {
   const [ slider ] = useState(ImageList);
 
   return (
     <>
     <Slide slider={slider} />
     <div className="grid-col">
-      {slider.map(f => <a key={f.key} onClick={() => history.push(`/detail/${f.props.alt}`)}>{f}</a>)}
+      {slider.map(f => <Link key={f.key} to={`/detail/${f.props.alt}`)}>{f}</Link>)}
     </div>
     </>
   );
