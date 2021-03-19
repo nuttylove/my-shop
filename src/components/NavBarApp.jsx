7285531edb5df3544da50c38ae45e30f51bd66cc
chotika <nutty.pansila@gmail.com>
2021-03-19 21:01:45 +0700

    position

--- src/components/NavBarApp.jsx
+++ src/components/NavBarApp.jsx
@@ -6,20 +6,17 @@ import { withRouter } from 'react-router-dom';
 const NavBarApp = ({ history }) => {
 	const { cart } = useSelector(state => state.app);
 	return (
-		<div className='navbar-header'>
-		<Navbar appearance='subtle'>
-			{/* <Navbar.Header className='flex items-center justify-center'>
-      	<img src={logo} width='50' height='50' alt='logo' />
-			</Navbar.Header> */}
+		<Navbar appearance='subtle' className='navbar-header'>
+			<Navbar.Header className='flex items-center justify-center'>
+      	<img src={logo} width='40' height='40' alt='logo' />
+			</Navbar.Header>
         <Navbar.Body>
-				<Nav><img src={logo} width='50' height='50' alt='logo' /></Nav>
 				<Nav pullRight>
 					<Nav.Item icon={<Icon icon='shopping-cart' size="2x" />} onClick={() => history.push('/cart')}>Cart ( {cart} )</Nav.Item>
 					<Nav.Item icon={<Icon icon='bars' size="2x" />} />
 				</Nav>
         </Navbar.Body>
 		</Navbar>
-		</div>
 	);
 };
 
