71cf7bbdbbbcbb06f1ed6f54a6856e10ce1468dd
chotika <nutty.pansila@gmail.com>
2021-03-19 11:23:39 +0700

    link

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -10,7 +10,7 @@ const ProductList = () => {
     <>
     <Slide slider={slider} />
     <div className="grid-col">
-      {slider.map(f => <Link key={f.key} to={`/detail/${f.props.alt}`)}>{f}</Link>)}
+      {slider.map(f => <Link key={f.key} to={`/detail/${f.props.alt}`}>{f}</Link>)}
     </div>
     </>
   );
